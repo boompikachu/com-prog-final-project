fdca30ef3d5bb7167f44125f2b6730e030e7c82c
Boom PT <37561295+boompikachu@users.noreply.github.com>
2019-11-21 22:14:23 +0700

    working todolist

--- src/app/pages/todo/todo.page.ts
+++ src/app/pages/todo/todo.page.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.page.scss'],
 })
 export class TodoPage implements OnInit {
+  taskList: string[] = [];
+  newTask: string = "";
 
   constructor() { }
-
+ 
   ngOnInit() {
   }
 
+  async addNewTask(task: string) {
+    await this.taskList.push(task);
+  }
+
+  async taskDone(index: number) {
+    await this.taskList.splice(index,1)
+  }
 }
