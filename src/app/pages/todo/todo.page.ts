9d2ce59e2af1adfca5ea27ca9e4802d159acd912
Boom PT <37561295+boompikachu@users.noreply.github.com>
2019-11-22 23:15:20 +0700

    Update todo.page.ts

--- src/app/pages/todo/todo.page.ts
+++ src/app/pages/todo/todo.page.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators'
 import { TodoProvider } from '../../../providers/provider';
-import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-todo',
@@ -23,22 +22,17 @@ export class TodoPage implements OnInit {
 
   getTodo() {
     this.onlineTask$ = this.todoProvider.getTodo();
-    // console.log("getTodoFromWeb")
-    // console.log(this.onlineTask$)
   }
 
   async postTodo(task: string) {
     this.todoProvider.postTodo(task).pipe(catchError(val => {
-      console.log('error but ok')
       this.getTodo()
       this.newTask = "";
       return of(val)
     })).subscribe(v => {
-      console.log('hihi', v)
       this.getTodo()
       this.newTask = "";
     });
-    console.log("add")
   }
 
 
