0db118a7a3847f559ef1603568410d92498e5d66
Boom PT <37561295+boompikachu@users.noreply.github.com>
2019-11-23 01:39:23 +0700

    Revert "?"
    This reverts commit 67360596f64474c0375d04b88ce8dd1a20dfcefa.

--- src/providers/provider.ts
+++ src/providers/provider.ts
@@ -25,12 +25,13 @@ export class TodoProvider {
     constructor(public http: HttpClient) {}
 
     getTodo() {
-        return this.http.get<any>(this.todoURL + "/get")
+        return this.http.get<any>("https://cors-anywhere.herokuapp.com/" + this.todoURL + "/get")
     }
     postTodo(task) {
-        return this.http.get<any>(this.todoURL + "/post?task_name=" + task)
+        console.log("prov")
+        return this.http.get<any>("https://cors-anywhere.herokuapp.com/" + this.todoURL + "/post?task_name=" + task)
     }
-    deleteTodo(task) {
+    deleteTodo(task: string) {
         return this.http.get<any>(this.todoURL + "/delete?task_name" + task)
     }
 }
